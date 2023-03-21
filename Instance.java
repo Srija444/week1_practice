937d47a4994ea4abb10e41c7c41adc7a3e6cc1d5
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-03-21 08:11:58 -0700

    Add files via upload

---
+++ Instance.java
@@ -0,0 +1,10 @@
+class Instance{
+  String s="magical stars";
+  public void universe(){
+    System.out.println("====  "+s+"  ====");
+  }
+  public static void main(String args[]){
+    Instance yes=new Instance();
+    yes.universe();
+  }
+}
