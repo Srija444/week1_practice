22e0a82174263417a68bfdf4843adddb3738e159
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-03-21 07:27:46 -0700

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+  public static void main(String[] args) {
+    int x, y, sum;
+    x = 4;
+    y = 7;
+    sum = x + y;
+    System.out.println("the sum of x and y is:" + sum);
+  }
+}
