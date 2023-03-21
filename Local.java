66cd2f9a85ac6bf7767dca4df2e4c730effe99f7
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-03-21 08:04:39 -0700

    Add files via upload

---
+++ Local.java
@@ -0,0 +1,10 @@
+class Local{
+  public void  She(){
+    int Age=18;
+    System.out.println("her age is:"+Age);
+  }
+ public static void main(String args[]){
+   Local her=new Local();
+   her.She();
+ }
+}
