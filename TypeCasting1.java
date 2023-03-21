4a4bd01e23be3d6962ffd690a42c089b876ba7e6
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-03-21 07:46:34 -0700

    Add files via upload

---
+++ TypeCasting1.java
@@ -0,0 +1,12 @@
+class TypeCasting1{
+  public static void main(String args[]){
+    String myString="magical stars";
+    for(int i=0;i<myString.length();i++){
+      char c=myString.charAt(i);
+      System.out.println("char at "+i+" index is:" +c);
+      
+    }
+    System.out.println("----              ----");
+     System.out.println(myString);
+  }
+}
