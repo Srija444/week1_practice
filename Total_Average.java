737432f3d3ed1a2e0ebe0878317d57437822a89a
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-03-21 07:45:59 -0700

    Add files via upload

---
+++ Total_Average.java
@@ -0,0 +1,10 @@
+class Total_Average {
+  public static void main(String args[]) {
+    int a = 10, b = 20, c = 30, total, average;
+    total = a + b + c;
+    average = total / 3;
+    System.out.println("the total of three subjects is:" + total);
+    System.out.println("the average of three subjects is:" + average);
+
+  }
+}
