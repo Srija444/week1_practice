85434b30b524fafec1f0ab9c2554abfa237901b8
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-03-21 07:28:37 -0700

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,15 @@
+class Arithmatic{
+  public static void main(String args[]){
+    int x=10,y=20,sum,mul,div,sub;
+    sum=x+y;
+    mul=x*y;
+    div=x/y;
+    sub=x-y;
+    System.out.println("the sum of x and y is:"+sum);
+    System.out.println("the mul of x and y is:"+mul);
+    System.out.println("the div of x and y is:"+div);
+    System.out.println("the sub of x and y is:"+sub);
+    
+    
+  }
+}
