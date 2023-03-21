a394020bf276ed27af45ccf1c8200721a797e85c
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-03-21 07:44:22 -0700

    Add files via upload

---
+++ Shopping.java
@@ -0,0 +1,11 @@
+class Shopping{
+  public static void main(String args[]){
+    int a=1000,b=2000,c=3000;
+    int employee_salary=20000,total,percentage;
+    total=a+b+c;
+    percentage=(total * 100)/employee_salary;
+    System.out.println("the total amount on shopping is:"+total);
+    System.out.println("the percentage is:"+percentage);
+    
+  }
+}
